fa28c95892f6af40f4d4389df8cdfd5fb49d90b0
wangrui <35493032@qq.com>
2018-11-28 10:37:11 +0800

    226

---
+++ 226.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 28/11/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root: return
+        root.left,root.right =  self.invertTree(root.right),self.invertTree(root.left)
+        return root
