30a6172c376cbd8ab029dafa4a48906558c942dd
wangrui <35493032@qq.com>
2018-11-28 10:30:26 +0800

    145 OK

---
+++ 145.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 28/11/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if root == None: return res
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.left: stack.append(curr.left)
+            if curr.right: stack.append(curr.right)
+        return res[::-1]
