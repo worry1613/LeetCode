8399a5e709d54fd43665b6566f4867e3938fb6f2
wangrui <35493032@qq.com>
2018-12-01 11:22:42 +0800

    28

---
+++ 28.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 1/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle: return 0
+        if not haystack: return -1
+        return haystack.find(needle)
