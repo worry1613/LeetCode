9c7e3b9d2f7d690a73abab6821fca280f3f24f0b
wangrui <35493032@qq.com>
2018-12-02 22:40:15 +0800

    35

---
+++ 35.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 2/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        elif target in nums:
+            return nums.index(target)
+        else:
+            if target < nums[0]:
+                return 0
+            elif target > nums[-1]:
+                return len(nums)
+            else:
+                for i in range(len(nums) - 1):
+                    if nums[i] < target < nums[i + 1]:
+                        return i + 1
