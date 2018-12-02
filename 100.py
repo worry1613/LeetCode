11efdd10e1bc725fa3964d7f6855080bfb060d7c
wangrui <35493032@qq.com>
2018-12-03 07:13:17 +0800

    100

---
+++ 100.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 3/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False  # Definition for a binary tree node.
