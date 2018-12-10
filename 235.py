754fb7d1ab861c1f6b9c62627a81b21f7ed191f6
wangrui <35493032@qq.com>
2018-12-10 10:02:27 +0800

    235

---
+++ 235.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 10/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root: return
+        big = max(p.val,q.val)
+        small = min(p.val,q.val)
+        if small <= root.val <= big:
+            return root
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
+        if l: return l
+        if r: return r
