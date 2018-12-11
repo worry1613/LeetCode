48ba844248d9742fb3326544d7b9dd6180bfb4bb
wangrui <35493032@qq.com>
2018-12-11 10:07:08 +0800

    349

---
+++ 349.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 11/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
