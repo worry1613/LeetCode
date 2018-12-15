b86bb3d4d7c438afb70023b9c3be868f4dda2cde
wangrui <35493032@qq.com>
2018-12-15 22:51:41 +0800

    429

---
+++ 429.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 15/12/2018 
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+import collections
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        res = []
+        que = collections.deque()
+        que.append(root)
+        while que:
+            level = []
+            size = len(que)
+            for _ in range(size):
+                node = que.popleft()
+                if not node:
+                    continue
+                level.append(node.val)
+                for child in node.children:
+                    que.append(child)
+            if level:
+                res.append(level)
+        return res
